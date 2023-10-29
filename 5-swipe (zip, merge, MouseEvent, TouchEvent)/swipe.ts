cad54c7f17c90cf8e5bc54230e95a578c800dbd7
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-10-29 21:35:05 +0300

    Выполнен рефакторинг функции getX - реализована возможность отслеживания swipe не только на Mobile, но и на Desktop. Использован оператор merge для объединения потоков, формируемых при swipe на разных устройствах (Mobile и Desktop). Созданы Observable, обрабатывающие swipe (mousedown и mouseup) на Desktop.

--- 5-swipe (zip, merge, MouseEvent, TouchEvent)/swipe.ts
+++ 5-swipe (zip, merge, MouseEvent, TouchEvent)/swipe.ts
@@ -1,4 +1,4 @@
-import { fromEvent, map, Observable, zip } from "rxjs";
+import { fromEvent, map, merge, Observable, zip } from "rxjs";
 
 /*Чтобы облегчить тестирование,
 нужно стремиться отделять pipe от Producer*/
@@ -11,15 +11,20 @@ import { fromEvent, map, Observable, zip } from "rxjs";
   .subscribe(v => console.log(v))*/
 
 //Определение координаты touchstart
-const swipeStartX$ = getX(fromEvent<TouchEvent>(document, 'touchstart'))
+const swipeStartX$ = getX(
+  fromEvent<TouchEvent>(document, 'touchstart'),
+  fromEvent<MouseEvent>(document, 'mousedown'))
 
 //Определение координаты touchend
-const swipeEndX$ = getX(fromEvent<TouchEvent>(document, 'touchend'))
+const swipeEndX$ = getX(
+  fromEvent<TouchEvent>(document, 'touchend'),
+  fromEvent<MouseEvent>(document, 'mouseup'))
 
 /*Объединяет несколько Observables для создания Observable,
 значения которого вычисляются по порядку значений каждого из его входных Observables.
 Оператор zip может быть использован для объединения значений нескольких Observables
-в единый flow.
+в единый flow, содержащий пары, тройки и т.д. значений, в зависимости от количества
+объединяемых Observables.
 */
 const swipes = zip(swipeStartX$, swipeEndX$)
 swipes
@@ -28,11 +33,16 @@ swipes
   })
 
 //Для удобства тестирования pipe нужно выносить в отдельную функции и передавать им flow
-function getX(sources$: Observable<TouchEvent>) {
-  return sources$
+function getX(source1$: Observable<TouchEvent>, source2$: Observable<MouseEvent>) {
+  return merge(source1$, source2$)
     .pipe(
-      map((event: TouchEvent) => event?.changedTouches[0].clientX)
-    )
+      map((event: TouchEvent | MouseEvent) => {
+          if (event instanceof TouchEvent) {
+            return event?.changedTouches[0].clientX
+          }
+          return event?.clientX
+        }
+      ))
 }
 
 function swipeDirection(sourses$: Observable<[number, number]>) {
@@ -42,7 +52,8 @@ function swipeDirection(sourses$: Observable<[number, number]>) {
         return coordX[0] > coordX[1] ? 'to left' : 'to right'
       })
     )
-    .subscribe(direction => console.log("Swipe direction - ", direction))
+    .subscribe(direction =>
+      console.log("Swipe direction - ", direction))
 }
 
 swipeDirection(swipes)
