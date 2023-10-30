40df62475b03403b164b6f65265a6fc264b2e90d
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-10-30 21:51:41 +0300

    Использован оператор combineLatest для объединения в массив последних значения каждого из полученных потоков.

--- src/slider.ts
+++ src/slider.ts
@@ -1,11 +1,17 @@
-import { fromEvent, map, Observable, tap } from "rxjs";
+import { combineLatest, fromEvent, map, Observable, tap } from "rxjs";
 
 const quality = fromEvent(document.getElementById('quality') as HTMLInputElement, 'change')
 const rating = fromEvent(document.getElementById('rating') as HTMLInputElement, 'change')
 const actual = fromEvent(document.getElementById('actual') as HTMLInputElement, 'change')
 
 //Если не подписаться на поток, то функция не будет вызывать изменений в DOM
-getRangeInputValue(quality).subscribe(value => console.log(value))
+const qualityValue =getRangeInputValue(quality)
+const ratingValue =getRangeInputValue(rating)
+const actualValue =getRangeInputValue(actual)
+
+//combineLatest - объединяет в массив последние значения каждого из полученных потоков
+const slideSequence$ = combineLatest([qualityValue,ratingValue,actualValue])
+  .subscribe(value => console.log(value))
 
 /**
  * Преобразует поток событий change в поток значений rangeInput,
