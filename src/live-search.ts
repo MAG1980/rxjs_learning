21de7a1664c96f07cc83b397584f314144d9bd63
MAG1980 <al-1980@inbox.ru>
2023-11-12 11:58:40 +0300

    Настроено формирование HTML из потока данных, поступающих с сервера, в ответ на запросы, отправляемые пользователем с клавиатуры.

--- src/live-search.ts
+++ src/live-search.ts
@@ -1,8 +1,29 @@
-import { debounceTime, distinctUntilChanged, filter, fromEvent, map, pluck, switchMap } from "rxjs";
+import {
+  bufferCount,
+  concatAll,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  Observable,
+  pluck, reduce,
+  switchMap
+} from "rxjs";
 import { subscribe } from "diagnostics_channel";
 import { ajax } from "rxjs/internal/ajax/ajax";
+import { AjaxResponse } from "rxjs/internal/ajax/AjaxResponse";
+
+export interface IResult {
+  body: string;
+  email: string;
+  id: number;
+  name: string
+  postId: number
+}
 
 const InputElement = document.getElementById('search') as HTMLInputElement
+const container = document.querySelector(".container") as HTMLDivElement
 fromEvent<KeyboardEvent>(InputElement, 'input')
   .pipe(
     debounceTime(300),
@@ -18,8 +39,44 @@ fromEvent<KeyboardEvent>(InputElement, 'input')
     switchMap((postId: number) => {
       return ajax(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
         .pipe(
-          map(httpResponse => httpResponse.response)
+          map<AjaxResponse<any>, IResult[]>((httpResponse: AjaxResponse<any>) => httpResponse.response),
+          //Преобразуем входной массив в поток, состоящий из элементов массива
+          concatAll(),
+          map((item: IResult) => createCard(item)),
+          //Объединяет элементы входного потока в группы
+          bufferCount(3),
+          //Формирование ряда карточек из массива HTML-строк
+          reduce((resultStr: string, htmlStr: string[]) => {
+            return resultStr += createRow(htmlStr)
+          }, ''),
+          map(resultStr => resultStr.trim().replace(/\s(<)/g, '<'))
         )
     })
   )
-  .subscribe(value => console.log(value))
+  .subscribe(htmlString => container.innerHTML=htmlString)
+
+export function request(source$: Observable<any>) {
+  return source$.pipe(
+    map(httpResponse => httpResponse.response)
+  )
+}
+
+export function createCard({ body, email, id, name, postId }: IResult) {
+  return `
+  <div class="col-md-4">
+        <div class="card">
+            <img src="https://placehold.co/600x400/orange/white" alt="" class="card-img-top">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <div class="card-text">${body}</div>
+        </div>
+    </div>
+  `
+}
+
+export function createRow(htmlStrings: string[]) {
+  return `<div class="row">
+${htmlStrings.join('')}
+</div>`
+}
